7ec09f880b7f23886383f4321f76304a1ed6d4e1
hemanthreddy44 <65831040+hemanthreddy44@users.noreply.github.com>
2021-11-19 14:55:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!!rockstar hemanth reddys edited"
+    return "Hello World!!rockstar hemanth reddys edited 3pm"
 
 
 if __name__ == "__main__":
